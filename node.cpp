cce1e6605fa21e488f655d71581090827340a317
Hammad Rana <154501207+hammadranaa@users.noreply.github.com>
2024-09-22 18:19:03 +0500

    Add files via upload

---
+++ node.cpp
@@ -0,0 +1,24 @@
+#include "node.h"
+
+node::node() {
+    val = 0;
+    next = NULL;
+}
+
+void node::set_val(int val) {
+    this->val = val;
+}
+
+int node::get_val() {
+    return val;
+}
+
+void node::set_next(node* temp) {
+    next = temp;
+}
+
+node* node::get_next() {
+    return next;
+}
+
+node::~node() {}
